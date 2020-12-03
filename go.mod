b27f5d436ca496031b36b632de8435973325a3e4
jpbrab0 <jpresende502@gmail.com>
2020-12-03 19:52:35 -0300

    Add Golang Obs Websocket and new commands.

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/gempir/go-twitch-irc
 go 1.15
 
 require (
+	github.com/christopher-dG/go-obs-websocket v0.0.0-20200720193653-c4fed10356a5 // indirect
 	github.com/gempir/go-twitch-irc/v2 v2.5.0 // indirect
 	github.com/joho/godotenv v1.3.0
 )
--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"fmt"
 	"os"
+    "time"
+    "log"
     "strings"
     "io/ioutil"
+    "math/rand"
     "github.com/joho/godotenv"
 	"github.com/gempir/go-twitch-irc/v2"
+    "github.com/christopher-dG/go-obs-websocket"
 )
 func getDotEnvVariable(key string) string {
 
@@ -21,19 +25,41 @@ func getDotEnvVariable(key string) string {
 }
 
 func main(){
+    rand.Seed(time.Now().UTC().UnixNano())
+    c := obsws.Client{Host: "localhost", Port: 4444}
+
+    if err := c.Connect(); err != nil {
+        log.Fatal(err)
+	}
+	defer c.Disconnect()
+
     tokenBot := getDotEnvVariable("TOKEN_BOT")
-    client := twitch.NewClient("brab0bot", tokenBot)
+    client := twitch.NewClient("brab0bot", tokenBot) 
+    client.OnConnect(func() {
+        client.Say("jpbrab0","O bot ta on!")
+    })
+    client.OnUserNoticeMessage(func(message twitch.UserNoticeMessage){
+        fmt.Println(message)
+    })
     client.OnPrivateMessage(func(message twitch.PrivateMessage){
         fmt.Println(message.User.Name+":"+ message.Message)
         if strings.HasPrefix(message.Message, "!ban"){
             nicknameBan := strings.Split(message.Message, "!ban")[1]
-            client.Say("jpbrab0",message.User.Name +  " baniu" + nicknameBan) 
+            client.Say("jpbrab0",message.User.Name +  " baniu" + nicknameBan)
+            numRandom := rand.Intn(1000 - 100) + 100
+            if(numRandom > 100 && numRandom < 499 ){
+                client.Say("jpbrab0", "/timeout "+message.User.Name+" 15")
+                client.Say("jpbrab0", message.User.Name+" foi banido(a)")
+            } else {
+                client.Say("jpbrab0", "/timeout "+nicknameBan+" 15")
+                client.Say("jpbrab0", nicknameBan+" foi banido(a)")
+            }
         } else if strings.HasPrefix(message.Message, "!discord"){
             nickname := strings.Split(message.Message, "!discord")[1]
             if(nickname == ""){
-                client.Say("jpbrab0", "Discord -> https://discord.gg/Sj4tQTb")
+                client.Say("jpbrab0", "Discord -> https://discord.io/caverna")
             } else {
-                client.Say("jpbrab0", nickname + "," + " Discord -> https://discord.gg/Sj4tQTb")
+                client.Say("jpbrab0", nickname + "," + " Discord -> https://discord.io/caverna")
             }
         } else if strings.HasPrefix(message.Message, "!os"){
             client.Say("jpbrab0", "Ubuntu 20.10 Shell: Zsh Theme: WhiteSur-dark")
@@ -59,4 +85,4 @@ func main(){
     })
     client.Join("jpbrab0")
     client.Connect()
-}
+}   
